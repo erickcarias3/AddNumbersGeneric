2b19be7a6fd4e911bc18b9922e6b87cbc7f68611
Erick Carias <Rick@pattys-MacBook-Pro-6.local>
2019-09-12 14:05:48 -0700

    second commit

--- src/com/assignment1/Client.java
+++ src/com/assignment1/Client.java
@@ -4,7 +4,7 @@ public class Client{
     public static void main(String [] args){
         int a = 10;
         double b=2.5;
-        long c = 45000;
+        long c = 450;
         float f = 0.1f;
 
 
@@ -14,7 +14,15 @@ public class Client{
         System.out.println("Test printing");
         System.out.println("Adding an Int and a Double");
         System.out.println("========================");
-        System.out.println("10 + 2.5 = " + newAdd.addNumbers(c,f) + "(Test)" );
+        System.out.println("10 + 2.5 = " + newAdd.addNumbers(a,b) + "(Test)" );
+
+
+        System.out.println("========================");
+        System.out.println("Test printing");
+        System.out.println("Adding a long and a float");
+        System.out.println("========================");
+        System.out.println("450 + 0.1 (float) = " + newAdd.addNumbers(c,f) + "(Test)" );
+
 
     }
 }
